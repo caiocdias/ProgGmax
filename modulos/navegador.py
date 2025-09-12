c0864c7928219faf3ccc3670ae54df58773a6690
Caio Cezar Dias <caiocd007@gmail.com>
2025-09-12 08:29:55 -0300

    Add slight delay after `_wait_global_ajax_idle` for improved stability

--- modulos/navegador.py
+++ modulos/navegador.py
@@ -443,7 +443,7 @@ class Navegador:
 
             # 4) espere o Ajax pós-alteração concluir antes de seguir
             self._wait_global_ajax_idle()
-
+            time.sleep(1)
             # 5) se o fluxo seguinte ainda precisar do iframe:
             WebDriverWait(self.driver, 10).until(
                 EC.frame_to_be_available_and_switch_to_it(
